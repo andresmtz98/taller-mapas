4edd4f86bdbc779144b46f46c11f3de9939b84f8
Andres Martinez <andresfmtz08@gmail.com>
2019-04-28 23:01:44 -0500

    Creación del proyecto RN

---
+++ App.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { SafeAreaView } from 'react-navigation';
+
+const styles = StyleSheet.create({
+ container: {
+   ...StyleSheet.absoluteFillObject,
+   flex: 1,
+ },
+ map: {
+   ...StyleSheet.absoluteFillObject,
+ },
+});
+
+export default () => (
+   <SafeAreaView style={styles.container}>
+     
+   </SafeAreaView>
+);
